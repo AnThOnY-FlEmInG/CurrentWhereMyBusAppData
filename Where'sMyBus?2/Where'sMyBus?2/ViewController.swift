f988734dce615d6333c39cc968cf7a2cce59faf0
Anthony Fleming <aaf5986@g.coppellisd.com>
2019-11-11 18:35:18 -0600

    Add files via upload

---
+++ Where'sMyBus?2/Where'sMyBus?2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Where'sMyBus?2
+//
+//  Created by Anthony Fleming on 10/31/19.
+//  Copyright © 2019 Anthony Fleming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBOutlet var getStarted: UIButton!
+    
+    @IBAction func getStartedPressed(_ sender: AnyObject) {
+            //switch view controller
+    }
+}
